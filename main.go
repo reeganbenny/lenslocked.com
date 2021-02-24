0656594892501cc50cd09ef0ba999351be938b67
Reegan Benny <reeganbenny@gmail.com>
2021-02-24 14:51:39 +0530

    Add xml file which contain more field and complex

--- main.go
+++ main.go
@@ -2,32 +2,106 @@ package main
 
 import (
 	"encoding/xml"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
+var washPostXML = []byte(`
+	<sitemapindex>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-politics-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-blogs-politics-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-opinions-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-blogs-opinions-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-local-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-blogs-local-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-sports-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-blogs-sports-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-national-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-blogs-national-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-world-sitemap.xml </loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-blogs-world-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-business-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-blogs-business-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-technology-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-blogs-technology-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-lifestyle-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-blogs-lifestyle-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-entertainment-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-blogs-entertainment-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-blogs-goingoutguide-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-goingoutguide-sitemap.xml</loc>
+		</sitemap>
+	</sitemapindex>
+`)
+
 //SitemapIndex is for Xml extraction
 type SitemapIndex struct {
-	Locataions []Location `xml:"sitemap"`
+	Locations []string `xml:"sitemap>loc"`
 }
 
-//Location is for xml extration
-type Location struct {
-	Loc string `xml:"loc"`
-}
-
-func (L Location) String() string {
-	return fmt.Sprintf(L.Loc)
+//News extracted from xml
+type News struct {
+	Titles []string `xml:"url>loc"`
 }
 
 func main() {
-	resp, _ := http.Get("https://www.washingtonpost.com/sitemaps/index.xml")
-	bytes, _ := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
-
 	var s SitemapIndex
+	var n News
+	bytes := washPostXML
 	xml.Unmarshal(bytes, &s)
 
-	fmt.Println(s.Locataions)
+	// fmt.Println(s.Locations)
+
+	for _, Location := range s.Locations {
+		// fmt.Println(Location)
+		resp, _ := http.Get(Location)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		xml.Unmarshal(bytes, &n)
+	}
+
+	// fmt.Println(n.Titles)
 }
