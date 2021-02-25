43cabac9f09623be3e8e8eee94633e76a70dd798
Reegan Benny <reeganbenny@gmail.com>
2021-02-25 14:58:01 +0530

    Since Keyword is not present in every news tag we have Remove Keyword NewsMap

--- main.go
+++ main.go
@@ -1,16 +1,122 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+var washPostXML = []byte(`
+	<sitemapindex>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-politics-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-blogs-politics-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-opinions-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-blogs-opinions-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-local-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-blogs-local-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-sports-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-blogs-sports-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-national-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-blogs-national-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-world-sitemap.xml </loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-blogs-world-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-business-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-blogs-business-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-technology-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-blogs-technology-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-lifestyle-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-blogs-lifestyle-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-entertainment-sitemap.xml</loc>
+		</sitemap>
+		<sitemap>
+			<loc>http://www.washingtonpost.com/news-blogs-entertainment-sitemap.xml</loc>
+		</sitemap>
+
+	</sitemapindex>
+`)
+
+// SitemapIndex is for Xml extraction
+type SitemapIndex struct {
+	Locations []string `xml:"sitemap>loc"`
+}
+
+//News extracted from xml
+type News struct {
+	Titles    []string `xml:"url>news>title"`
+	Locations []string `xml:"url>loc"`
+}
+
+//Keywords struct
+type Keywords struct {
+	Keyword string
+}
+
+//NewsMap is the value and Title will be the key
+type NewsMap struct {
+	// Keyword  string
+	Location string
 }
 
 func main() {
-	http.HandleFunc("/path", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	var s SitemapIndex
+	var n News
+	newsmap := make(map[string]NewsMap)
 
+	bytes := washPostXML
+	xml.Unmarshal(bytes, &s)
+	i := 1
+	for _, Location := range s.Locations {
+		resp, _ := http.Get(Location)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		xml.Unmarshal(bytes, &n)
+		for idx := range n.Titles {
+			newsmap[n.Titles[idx]] = NewsMap{n.Locations[idx]}
+		}
+	}
+
+	for idx, data := range newsmap {
+		fmt.Println("i = ", i)
+		i = i + 1
+		fmt.Println("\n\n\nTitle := ", idx)
+		fmt.Println("\nLocation =", data.Location)
+	}
 }
